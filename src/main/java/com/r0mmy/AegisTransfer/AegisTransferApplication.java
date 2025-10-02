954f8ab8cf5cc3ef1056748c28c83499be70f89e
Roman <roma_lobanov@inbox.ru>
2025-10-02 17:01:16 +0300

    created

---
+++ src/main/java/com/r0mmy/AegisTransfer/AegisTransferApplication.java
@@ -0,0 +1,13 @@
+package com.r0mmy.AegisTransfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AegisTransferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AegisTransferApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/r0mmy/AegisTransfer/AegisTransferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.r0mmy.AegisTransfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AegisTransferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
